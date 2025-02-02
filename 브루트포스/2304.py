2ea57ff61512e3b5f82c23573877b34a85909926
kimjunha1231 <tto9011@naver.com>
2025-02-02 21:09:15 +0900

    2025.02.02 완

---
+++ 브루트포스/2304.py
@@ -0,0 +1,43 @@
+import sys
+
+N = int(sys.stdin.readline())
+container = []
+
+
+for i in range(N):
+    container.append(list(map(int, sys.stdin.readline().split())))
+
+container.sort(key=lambda x: x[0])
+idx = 0
+max_height = max(container, key=lambda x: x[1])[1]
+for q in range(N):
+    if max_height == container[q][1]:
+        idx = q
+
+
+result = 0
+height = container[0]
+
+
+if N == 1:
+    print(container[0][1])
+else:
+    for j in range(1, idx):
+        if height[1] < container[j][1]:
+            result += (container[j][0] - height[0]) * height[1]
+            height = container[j]
+
+    result += (container[idx][0] - height[0]) * height[1]
+
+    end_idx = idx
+    for n in range(idx, N):
+        if max_height == container[n][1]:
+            end_idx = n
+    result += (container[end_idx][0] - container[idx][0] + 1) * max_height
+    height = container[-1]
+    for k in range(N - 1, end_idx, -1):
+        if height[1] < container[k][1]:
+            result += (height[0] - container[k][0]) * height[1]
+            height = container[k]
+    result += abs(container[end_idx][0] - height[0]) * height[1]
+print(result)
--- 자료구조/2493.py
+++ 자료구조/2493.py
@@ -1,1 +1,22 @@
 import sys
+
+
+N = int(sys.stdin.readline())
+
+top_list = list(map(int, sys.stdin.readline().split()))
+result = []
+stack = []
+
+for i in range(N):
+    while stack:
+        if stack[-1][0] < top_list[i]:
+            stack.pop()
+
+        else:
+            result.append(stack[-1][1] + 1)
+
+            break
+    if not stack:
+        result.append(0)
+    stack.append([top_list[i], i])
+print(" ".join(map(str, result)))
