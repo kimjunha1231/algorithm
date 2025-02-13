a6e820c98f724df57b06332fa6637f7d2e9d848a
kimjunha1231 <tto9011@naver.com>
2025-02-13 23:54:31 +0900

    2025.02.13

--- 자료구조/스택/10799.py
+++ 자료구조/스택/10799.py
@@ -3,11 +3,17 @@ import sys
 stack = []
 n_list = sys.stdin.readline().strip()
 count = 0
-for i in n_list:
-    if i == "(":
-        count += 1
+
+
+for i in range(len(n_list)):
+    if n_list[i] == "(":
         stack.append(i)
     else:
-        count += 1
+
         stack.pop()
+        if n_list[i - 1] == "(":
+            count += len(stack)
+        else:
+            count += 1
+
 print(count)
